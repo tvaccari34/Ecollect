f19ae3e83ea63e47d03d3a298e256b052bb28ab9
unknown <tiago.vaccari@outlook.com>
2020-06-07 12:01:16 +0100

    Image Upload created

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -6,12 +9,29 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 routes.get('/items', itemsController.index);
-
-routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+
+routes.post(
+    '/points', 
+    upload.single('image') , 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.string().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required(),
+            items: Joi.string().required(),
+        })
+    }, { abortEarly: false }),
+    pointsController.create
+);
 
 
 export default routes;
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -7,6 +7,8 @@ import api from '../../services/api';
 import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
 
+import DropZone from '../../components/dropzone';
+
 import logo from '../../assets/logo.svg';
 
 interface Item {
@@ -31,6 +33,7 @@ const CreatePoint = () => {
     const [ cities, setCities ] = useState<string[]>([]);
     const [ selectedPosition, setSelectedPosition ] = useState<[number, number]>([0, 0]);
     const [ selectedItems, setSelectedItems ] = useState<number[]>([])
+    const [ selectedFile, setSelectedFile ] = useState<File>();
     
 
     const [formData, setFormData] = useState({
@@ -129,17 +132,21 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        }
+        const data = new FormData()
 
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+        
+        if(selectedFile){
+            data.append('image', selectedFile)
+        } 
+        
         await api.post('points', data);
 
         alert('Ponto de coleta criado');
@@ -158,6 +165,8 @@ const CreatePoint = () => {
             </header>
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de coleta</h1>
+
+                <DropZone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
